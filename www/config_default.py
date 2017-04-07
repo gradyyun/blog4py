c0bd25ded01bfb78c3c0143036868a1134f902a3
styletodo <styletodo@gmail.com>
2017-04-07 19:04:55 +0800

    add user login

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding:utf-8
+#config_default.py
+
+__author__ = 'HSING LI'
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'root.org',
+        'database': 'blog'
+    },
+    'session': {
+        'secret': 'BlOg4Py'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#coding:utf-8
+#config_override.py
+
+__author__ = 'HSING LI'
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '192.168.204.201'
+    }
+}
