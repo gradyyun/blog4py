6a37da5b1145ee16fa06dd32bdb806b08c32f509
styletodo <styletodo@gmail.com>
2017-04-10 19:19:48 +0800

    add blog comment

--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -38,7 +38,7 @@ db.create_engine(**configs.db)
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
-template_engine.add_filter('datatime', datetime_filter)
+template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
